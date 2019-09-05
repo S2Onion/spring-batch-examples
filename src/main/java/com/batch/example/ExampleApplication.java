30d91ad400fbc980fd1fe0ef4af0963a988bf18d
S2Onion <ksskss665@naver.com>
2019-09-05 23:26:36 +0900

    - Parameter 실행할 Job 정보 전달받음 - jobLauncher로 Job 실행

--- src/main/java/com/batch/example/ExampleApplication.java
+++ src/main/java/com/batch/example/ExampleApplication.java
@@ -1,20 +1,41 @@
 package com.batch.example;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-
-/**
- * @author : S2Onion
- */
+/** @author : S2Onion */
 @Log4j2
 @SpringBootApplication
 @EnableBatchProcessing
 public class ExampleApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ExampleApplication.class, args);
+
+    if (args.length < 1) {
+      throw new IllegalArgumentException("Parameter Job is necessary");
+    }
+
+    try (ConfigurableApplicationContext ctx =
+        SpringApplication.run(ExampleApplication.class, args)) {
+      ExampleBatchLauncher exampleBatchLauncher =
+          ctx.getBean("exampleBatchLauncher", ExampleBatchLauncher.class);
+
+      String jobName = args[0];
+      System.setProperty("job.name", jobName);
+      Job job = ctx.getBean(jobName, Job.class);
+      JobParametersBuilder jobParametersBuilder =
+          new JobParametersBuilder()
+              .addString("jobName", jobName)
+              .addString("runningTime", LocalDateTime.now(ZoneId.of("GMT")).toString());
+
+      exampleBatchLauncher.start(job, jobParametersBuilder.toJobParameters());
+    }
   }
 }
